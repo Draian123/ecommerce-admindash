a962e6629b56b1934751240bb4a7f79085cd2d37
Draian123 <beiteldennis3@yahoo.de>
2023-06-11 11:59:35 +0200

    add new product

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+const { Schema, model, models } = require("mongoose");
+
+const ProductSchema = new Schema({
+    title: {type: String, required: true},
+    descripton: String,
+    price: {type: Number, required: true},
+})
+
+export const Product = models.Product || model('Product', ProductSchema)
